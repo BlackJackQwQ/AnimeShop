23b8a41f435a1ebfe91ddbbee680ae3d52447eff
BlackJackQwQ <AssassinXYZ399@gmail.com>
2026-05-03 19:31:08 +0700

    Fix: Track setup.sql in git (was excluded by gitignore whitelist)

---
+++ setup.sql
@@ -0,0 +1,4 @@
+USE anime_shop;
+UPDATE wp_options SET option_value = 'a:1:{i:0;s:24:"anime-shop/anime-shop.php";}' WHERE option_name = 'active_plugins';
+UPDATE wp_options SET option_value = 'http://localhost:8080' WHERE option_name = 'siteurl';
+UPDATE wp_options SET option_value = 'http://localhost:8080' WHERE option_name = 'home';
